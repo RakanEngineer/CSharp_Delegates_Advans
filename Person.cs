c94d08c4754fa568816fc2f61c3ba4d5284fa9bf
RakanEngineer <rakan.engineer@gmail.com>
2024-10-26 23:48:57 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Delegates_Advans
+{
+    class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string SocialSecurityNumber { get; }
+
+        public Person(string firstName, string lastName, string socialSecurityNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            SocialSecurityNumber = socialSecurityNumber;
+        }
+
+        //public byte GetAge()
+        //{
+        //    string datePart = SocialSecurityNumber.Substring(0, 6);
+
+        //    DateTime birthDate = DateTime.ParseExact(datePart, "yyMMdd", System.Globalization.CultureInfo.InvariantCulture);
+
+        //    TimeSpan timeSpan = DateTime.Today - birthDate;
+
+        //    byte years = (byte)(timeSpan.Days / 365);
+
+        //    return years;
+        //}
+        public byte Age
+        {
+            get
+            {
+                string datePart = SocialSecurityNumber.Substring(0, 6);
+
+                DateTime birthDate = DateTime.ParseExact(datePart, "yyMMdd", System.Globalization.CultureInfo.InvariantCulture);
+
+                TimeSpan timeSpan = DateTime.Today - birthDate;
+
+                byte years = (byte)(timeSpan.Days / 365);
+
+                return years;
+            }
+
+        }
+        //public bool IsFemale()
+        //{
+        //    var genderNumber = int.Parse(SocialSecurityNumber.Substring(9, 1));
+
+        //    return genderNumber % 2 == 0;
+        //}
+        public bool IsFemale
+        {
+            get
+            {
+                var genderNumber = int.Parse(SocialSecurityNumber.Substring(9, 1));
+
+                return genderNumber % 2 == 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Text;
+using static System.Console;
+
+namespace CSharp_Delegates_Advans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> personList = new List<Person>();
+            personList.Add(new Person("Jane", "Doe", "820101-1020"));
+            personList.Add(new Person("John", "Doe", "800101-2010"));
+            personList.Add(new Person("Jim", "Doe", "780101-2010"));
+            personList.Add(new Person("Jessica", "Doe", "900101-1020"));
+            personList.Add(new Person("Jake", "Doe", "900101-2010"));
+            personList.Add(new Person("Janice", "Doe", "920101-1020"));
+
+            //Console.WriteLine(personList);
+
+            //foreach (Person person in personList.Where(IsUnderThirty))
+            //{
+            //    WriteLine($"{person.FirstName} {person.LastName}, {person.Age}");
+            //}
+
+            foreach (Person person in personList)
+            {
+                //if (person.Age < 30) continue;
+                WriteLine($"{person.FirstName} {person.LastName}, {person.Age}");
+            }
+            //foreach (Person person in personList.Where(person => person.IsFemale && person.Age < 30))
+            //{
+            //    WriteLine($"{person.FirstName} {person.LastName}, {person.Age}");
+            //}
+
+            //Func<Person, bool> myFunc = person => person.IsFemale && person.Age < 30;
+
+            //// person => person.IsFemale && person.Age < 30
+            //foreach (Person person in personList.Where(myFunc))
+            //{
+            //    WriteLine($"{person.FirstName} {person.LastName}, {person.Age}");
+            //}
+        }
+
+        static bool IsUnderThirty(Person x)
+        {
+            return x.Age < 30;
+        }
+        //static bool IsUnderThirty(Person person)
+        //{
+        //    return person.Age < 30;
+        //}
+        static bool IsFemaleUnderThirty(Person person)
+        {
+            return person.IsFemale && IsUnderThirty(person);
+
+        }
+        static bool IsMaleUnderThirty(Person person)
+        {
+            return !person.IsFemale && IsUnderThirty(person);
+
+        }
+    }
+}
